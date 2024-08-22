84b8a2546a60dfd84744b371f5b6fbcc90c50b46
Maxwell Henderson <maxwellc.henderson@gmail.com>
2024-08-22 17:43:44 -0400

    course error message updates + Authenticator page UI updates + Authentication instructions added

--- backend/functions/chunk-handler/utils/response-maps/courseResponseMap.mjs
+++ backend/functions/chunk-handler/utils/response-maps/courseResponseMap.mjs
@@ -3,11 +3,23 @@ const courseResponseMap = (response, data) => {
   // Create a map of code to catalog items
 
   const responseData = response.data.map((item, index) => {
+    let message;
+    console.log("item", item);
+    console.log("item.message", item.message);
+    // Check if the message contains a PHP-like array structure
+    if (item.message.includes("Array")) {
+      console.log("Handling PHP-like array structure");
+      console.log("item.message", item.message);
+      message = "Description cannot be blank";
+    } else {
+      message = item.message;
+    }
+
     return {
       row_index: index + 1,
       course_name: data[index].course_name,
       success: item.success,
-      message: item.message,
+      message: message,
     };
   });
   return responseData;
